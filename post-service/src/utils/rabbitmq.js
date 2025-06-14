d6f0fa722ca381043094e535e8252db7081595ac
Jeet Sharma <jeetsharma31179@gmail.com>
2025-06-14 23:19:32 +0530

    rabbitmq code commented due to TCP error

--- post-service/src/utils/rabbitmq.js
+++ post-service/src/utils/rabbitmq.js
@@ -1,43 +1,43 @@
-const logger = require("./logger");
-const amqp = require("amqplib");
+// const logger = require("./logger");
+// const amqp = require("amqplib");
 
-let channel = null;
-let connection = null;
+// let channel = null;
+// let connection = null;
 
-const EXCHANGE_NAME = "post_event";
+// const EXCHANGE_NAME = "post_event";
 
-const connectRabbitMQ = async () => {
-    try {
-        connection = await amqp.connect(process.env.RABBITMQ_URL);
-        channel = await connection.createChannel();
+// const connectRabbitMQ = async () => {
+//     try {
+//         connection = await amqp.connect(process.env.RABBITMQ_URL);
+//         channel = await connection.createChannel();
 
-        await channel.assertExchange(EXCHANGE_NAME, "topic", {
-            durable: false,
-        });
+//         await channel.assertExchange(EXCHANGE_NAME, "topic", {
+//             durable: false,
+//         });
 
-        logger.info("Connected to RabbitMQ successfully");
+//         logger.info("Connected to RabbitMQ successfully");
 
-        return channel;
-    } catch (error) {
-        logger.error("Failed to connect to RabbitMQ", error);
-        throw error;
-    }
-}
+//         return channel;
+//     } catch (error) {
+//         logger.error("Failed to connect to RabbitMQ", error);
+//         throw error;
+//     }
+// }
 
-async function publishEvent(routingKey, message) {
-    try {
-        if (!channel) {
-            throw new Error("RabbitMQ channel is not initialized");
-        }
+// async function publishEvent(routingKey, message) {
+//     try {
+//         if (!channel) {
+//             throw new Error("RabbitMQ channel is not initialized");
+//         }
 
-        const msgBuffer = Buffer.from(JSON.stringify(message));
-        channel.publish(EXCHANGE_NAME, routingKey, msgBuffer);
+//         const msgBuffer = Buffer.from(JSON.stringify(message));
+//         channel.publish(EXCHANGE_NAME, routingKey, msgBuffer);
 
-        logger.info(`Event published with routing key: ${routingKey}`);
-    } catch (error) {
-        logger.error("Failed to publish event", error);
-        throw error;
-    }
-}
+//         logger.info(`Event published with routing key: ${routingKey}`);
+//     } catch (error) {
+//         logger.error("Failed to publish event", error);
+//         throw error;
+//     }
+// }
 
-module.exports = { connectRabbitMQ , publishEvent };
+// module.exports = { connectRabbitMQ , publishEvent };
